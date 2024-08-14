7b1d61a676e58b10798e2bbd9953e6471a4f77a2
iamena <161711918+iamena@users.noreply.github.com>
2024-08-14 09:02:41 -0300

    Amenaídes 2B

---
+++ computação gráfica Amenaídes/Teste.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+
+def evaluate (event):
+    result_label.config(text=str(eval(entry.get())))
+
+root = tk.Tk()
+root.title("Calculadora Simples")
+
+entry = tk.Entry(root)
+entry.bind("<Return>", evaluate)
+entry.pack()
+
+result_label =  tk.Label (root, text="")
+result_label.pack()
+
+root.geometry("250x100")
+root.mainloop()
